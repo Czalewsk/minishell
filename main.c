8b82156c94d96b2b3b311f2b41a0cd5c8308a872
Dricc <czalewsk@e2r9p12.42.fr>
2017-09-27 20:28:52 +0200

    trop de test today....' '

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/24 17:05:49 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/27 15:23:13 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/27 20:18:06 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,46 @@
 
 static volatile char	g_err;
 
+void	ms_signal_handler(int sig)
+{
+	g_err = 1;
+	ft_printf("sig(%i)=%b\n", sig, sig);
+	close(1);
+	if (signal(SIGINT, ms_signal_handler) == SIG_ERR)
+		ft_printf("Error in signal fucntion\n");
+}
+
 int		main(int ac, char **av, char **env)
 {
 	char	*line;
 	pid_t	father;
 	char	**cmd;
 	int		i;
+	int		ret;
 
-	(void)ac;
-	(void)av;
 	i = 0;
+	if (signal(SIGINT, ms_signal_handler) == SIG_ERR)
+		ft_printf("Error in signal fucntion\n");
 	while (env[i])
 		ft_printf("%s\n", env[i++]);
-	while (ft_printf("$>") && get_next_line(0, &line) > 0)
+	while (!ac || av)
 	{
+		ret = 0;
 		g_err = 0;
+		ft_printf("$>");
+		if (get_next_line(0, &line) < 0)
+			break ;
 		cmd = ft_strsplit(line, ' ');
 		father = fork();
 		if (!father)
 		{
-			execve(*cmd, (cmd), env);
-			g_err = ft_printf("Fail to launch the command\n");
+			g_err = execve(*cmd, (cmd), env);
+			ft_printf("Fail command(g_err=%i)\n", g_err);
+			exit(1);
 		}
 		else if (father > 0)
-		{
-			wait(NULL);
-		}
-		!g_err ? ft_printf("Commande fini\n") : 0;
+			ret = wait(&father);
+		!ret ? ft_printf("Successful command\n") : 0;
 	}
+	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/24 17:01:17 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/24 17:17:18 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/27 16:54:47 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "./libft/libft.h"
 # include <unistd.h>
 # include <sys/types.h>
+# include <sys/signal.h>
 
 #endif
