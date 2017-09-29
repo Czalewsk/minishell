35195b8fc25d742292bd64929ce0acc3e2b0bc11
Dricc <czalewsk@e2r9p12.42.fr>
2017-09-29 09:37:17 +0200

    main and header done

--- minishell.c
+++ minishell.c
@@ -6,13 +6,38 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/28 19:36:14 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/28 19:48:35 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/29 09:36:48 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int			main(int ac, char **av, char **env)
+volatile char	g_prpt_display = 0;
+
+int			print_usage(char *name)
 {
-	
+	ft_putstr(name);
+	ft_putendl(": No parameters needed");
+	return (1);
+}
+
+int				main(int ac, char **av, char **env)
+{
+	char			*line;
+	char			**cmd;
+	unsigned char	ret;
+
+	if (ac > 1)
+		return(print_usage(*av));
+	ms_init_sgnl_hdlr();
+	ms_init_env(&env);
+	while (av)
+	{
+		g_prpt_display = ms_print_prompt();
+		if (!ms_read_line(&line))
+			break ;
+		g_prpt_display = 0;
+		if (ms_interpreter(&line, env) && (ret = ms_execute(cmd, env)))
+			ms_print_exit_status(ret);
+	}
 }
--- minishell.h
+++ minishell.h
@@ -6,11 +6,28 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/24 17:01:17 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/24 17:01:51 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/29 09:36:47 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __MINISHELL_H
 # define __MINISHELL_H
+
+# include "./libft/libft.h"
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/signal.h>
+# include <sys/wait.h>
+# include <limits.h>
+
+# define PROMPT "$>"
+
+void		ms_init_sgnl_hdlr(void);
+void		ms_init_env(char ***env);
+char		ms_print_prompt(void);
+char		ms_read_line(char **line);
+char		ms_interpreter(char **line, char **env);
+char		ms_execute(char **cmd, char **env);
+void		ms_print_exit_status(unsigned char ret);
 
 #endif
