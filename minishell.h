11e51cbaab1d71a8c0a05e97fd96bcfee38933bb
Dricc <czalewsk@e1r13p12.42.fr>
2017-09-30 20:39:14 +0200

    update header and proto

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/24 17:01:17 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/30 19:49:20 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/30 20:37:42 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,16 @@
 
 extern volatile char	g_prpt_display;
 
-void		ms_init_sgnl_hdlr(void);
-void		ms_env_init(char ***env);
-char		ms_print_prompt(char ***env);
-char		ms_read_line(char **line);
-char		***ms_interpreter(char **line);
-char		ms_execute(char ***cmd, char **env);
-void		ms_print_exit_status(unsigned char ret);
+void			ms_init_sgnl_hdlr(void);
+void			ms_env_init(char ***env);
+char			ms_print_prompt(char ***env);
+char			ms_read_line(char **line);
+char			***ms_interpreter(char **line);
+void			ms_execute(char ***cmd, char **env);
+void			ms_print_exit_status(unsigned char ret);
+char			ms_check_is_builtin(char **cmd, char **env);
+void			ms_builtin_wrap(char **exec, char **env);
+char			ms_check_bin(char **exec, char **env);
+unsigned char	ms_exec_bin(char **exec, char **env);
 
 #endif
--- ms_execute.c
+++ ms_execute.c
@@ -6,26 +6,30 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/30 20:01:39 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/09/30 20:13:19 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/09/30 20:38:28 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char		ms_execute(char ***cmd, char **env)
+void		ms_print_exit_statut(char *exec, unsigned char status)
 {
-	int		i;
-	int		j;
-	char	**exec;
-	
+
+}
+
+void		ms_execute(char ***cmd, char **env)
+{
+	int				i;
+	char			**exec;
+	unsigned char	ret;
 
 	i = -1;
-	j = -1;
 	while ((exec = *(cmd + ++i)))
 	{
 		if (ms_check_is_builtin(exec, env))
 			ms_builtin_wrap(exec, env);
 		else if (ms_check_bin(exec, env))
-			ms_exec_bin(exec, env);
+			if ((ret = ms_exec_bin(exec, env)))
+				ms_print_exit_statut(*exec, ret);
 	}
 }
